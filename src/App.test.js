2d3f722620274ee900105f6b703856505c32f48e
Sam Chen <sam8211112222@gmail.com>
2023-05-08 19:08:36 +0800

    modify test

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from "./App";
 
 test("renders learn react link", () => {
   render(<App />);
-  const linkElement = screen.getByText(/蔡英文下台/i);
+  const linkElement = screen.getByText(/I was changed on the feature branch/i);
   expect(linkElement).toBeInTheDocument();
 });
